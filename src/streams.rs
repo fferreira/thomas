306fb4d7f4ace671acc95e4d27eebe5804ce83e1
Francisco Ferreira <fferreira@users.noreply.github.com>
2023-11-15 11:06:38 +0000

    remove trait methods that we don't need

--- src/streams.rs
+++ src/streams.rs
@@ -4,6 +4,4 @@ pub trait Stream {
     type Item: PartialEq + std::fmt::Debug;
     //FIXME: Debug is only needed for the tests, if I write them
     fn next(&self) -> Option<(&Self, &Self::Item)>;
-    fn peek(&self) -> Option<&Self::Item>;
-    fn drop(&self) -> Option<&Self>;
 }
