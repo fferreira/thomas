688b8de810e2468f705f2035146287b2b9b0209b
Francisco Ferreira <fferreira@users.noreply.github.com>
2023-11-17 13:29:29 +0000

    more tests

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,18 @@ mod tests {
     }
 
     #[test]
-    fn parse_many_a_or_b() {
+    fn parse_many_a_or_b_zero() {
+        use super::*;
+        let mut input = "".chars();
+        let mut grammar = Grammar::new();
+        grammar.insert("AORB".to_string(), Rule::ZeroOrMore(Box::new(Rule::Choice(vec![Rule::Terminal('a'), Rule::Terminal('b')]))));
+        let (rest, cst) = parse(&grammar, "AORB", &mut input).unwrap();
+        assert_eq!(rest.clone().next(), None);
+        assert_eq!(cst, None);
+    }
+
+    #[test]
+    fn parse_many_a_or_b_one() {
         use super::*;
         let mut input = "b".chars();
         let mut grammar = Grammar::new();
@@ -38,5 +49,16 @@ mod tests {
         let (rest, cst) = parse(&grammar, "AORB", &mut input).unwrap();
         assert_eq!(rest.clone().next(), None);
         assert_eq!(cst, Some(CST::Node("AORB".to_string(), Box::new(CST::Terminal(&'b')))));
+    }
+
+    #[test]
+    fn parse_many_a_or_b_more() {
+        use super::*;
+        let mut input = "bab".chars();
+        let mut grammar = Grammar::new();
+        grammar.insert("AORB".to_string(), Rule::ZeroOrMore(Box::new(Rule::Choice(vec![Rule::Terminal('a'), Rule::Terminal('b')]))));
+        let (rest, cst) = parse(&grammar, "AORB", &mut input).unwrap();
+        assert_eq!(rest.clone().next(), None);
+        assert_eq!(cst, Some(CST::Node("AORB".to_string(), Box::new(CST::Sequence(vec![CST::Terminal(&'b'), CST::Terminal(&'a'), CST::Terminal(&'b')])))));
     }
 }
